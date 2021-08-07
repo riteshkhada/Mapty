c2ba04af0d9690946fd81b79eefb27a4b8848f90
riteshkhadka <khadkaritesh45@gmail.com>
2021-08-08 01:06:26 +0545

    commit

--- script.js
+++ script.js
@@ -33,16 +33,30 @@ if (navigator.geolocation)
       const coords = [latitude, longitude];
 
       const map = L.map("map").setView(coords, 7);
-
+      console.log(map);
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent);
+
+        const { lat, lng } = mapEvent.latlng;
+        L.marker(coords)
+          .addTo(map)
+          .bindPopup(
+            l.popup({
+              maxwidth: 300,
+              minwidth: 200,
+              autoclose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("workout")
+          .openPopup();
+      });
     },
 
     function () {
