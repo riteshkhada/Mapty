f0eb158a2452b13248ead6c7f0e734cbe5ce0d83
riteshkhadka <khadkaritesh45@gmail.com>
2021-08-07 20:06:47 +0545

    commit

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
+];
+const workouts = document.querySelector(".workouts");
+const form = document.querySelector("form");
+const type = document.querySelector(".form__input--type");
+const distance = document.querySelector(".form__input--distance");
+const duration = document.querySelector(".form__input--duration");
+const cadence = document.querySelector(".form__input--cadence");
+const elevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map("map").setView(coords, 7);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+    },
+
+    function () {
+      alert(`You could not get accessed!`);
+    }
+  );
